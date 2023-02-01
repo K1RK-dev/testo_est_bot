21dddacfa1062e780a856cbf93a33eb25f9b9b58
Kirill <79369009+K1RK-dev@users.noreply.github.com>
2023-02-01 22:10:33 +0300

    New Version(Production)
    1)Добавлена метка бота для фп
    2)Исправлен бег с неполной каруселью
    3)Изменение ответов бота

--- src/helpers/product_menu_loader.py
+++ src/helpers/product_menu_loader.py
@@ -14,15 +14,11 @@ async def generate_carousels():
     items_count, keyboard_index = 0, -1
 
     for menu_item in product_menu:
-        if items_count == 10:
-            carousel = json.dumps({"type": "carousel", "elements": templates}, ensure_ascii=False).encode('utf-8').decode()
-            carousels.append(carousel)
-            items_count = 0
-            templates = []
 
         kb = (Keyboard(one_time=False, inline=True)
               .add(Text('Добавить в корзину', payload={'add_to_cart': menu_item['article']}), color=KeyboardButtonColor.POSITIVE)
               .add(Text('Состав', payload={'get_ingredients': menu_item['article']}), color=KeyboardButtonColor.POSITIVE))
+
         keyboards.append(kb)
         keyboard_index += 1
 
@@ -32,4 +28,11 @@ async def generate_carousels():
                              buttons=keyboards[keyboard_index].get_json())
         items_count += 1
         templates.append(te.raw)
+
+        if items_count == 10 or items_count == len(product_menu):
+            carousel = json.dumps({"type": "carousel", "elements": templates}, ensure_ascii=False).encode('utf-8').decode()
+            carousels.append(carousel)
+            items_count = 1
+            templates = []
+
     return carousels
